25e798a2e2443f03a27069e90a0ebc32f2c8a867
Galgallo Wako <galgallowako254@gmail.com>
2017-10-20 12:09:26 +0300

    [Feature]Implementation of Car object class

--- car.py
+++ car.py
@@ -1,14 +1,15 @@
 class Car(object):
   
   def __init__(self, name="General", model="GM", Type = None, 
-  												 num_of_doors = 4,
-  												 num_of_wheels = 4,
-  												 speed = 0):
+  												                       num_of_doors = 4,
+  												                       num_of_wheels = 4,
+  												                       speed = 0):
     self.model = model
     self.name = name
     self.Type = Type
     self.num_of_doors = num_of_doors
     self.num_of_wheels = num_of_wheels
+    self.speed = speed
     if self.name == "Porshe" or self.name == "Koenigsegg":
     	self.num_of_doors = 2
     if self.Type == "trailer":
