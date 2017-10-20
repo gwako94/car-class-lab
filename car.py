097904a7b5ff898ae53336d5f07eb81587e38afd
gwako94 <galgallowako254@gmail.com>
2017-10-20 12:53:37 +0300

    Updated drive method

--- car.py
+++ car.py
@@ -1,14 +1,16 @@
 class Car(object):
   
-  def __init__(self, name="General", model="GM", Type = None, 
-  												 num_of_doors = 4,
-  												 num_of_wheels = 4,
-  												 speed = 0):
+  def __init__(self, name="General", model="GM", 
+                                     Type = "saloon", 
+  												           num_of_doors = 4,
+  												           num_of_wheels = 4,
+  												           speed = 0):
     self.model = model
     self.name = name
     self.Type = Type
     self.num_of_doors = num_of_doors
     self.num_of_wheels = num_of_wheels
+    self.speed = speed
     if self.name == "Porshe" or self.name == "Koenigsegg":
     	self.num_of_doors = 2
     if self.Type == "trailer":
@@ -17,3 +19,13 @@ class Car(object):
   	if self.Type != "trailer":
   		return True
   	return False
+
+  def drive(self,speed):
+    if speed == 7 and self.Type == "trailer":
+      self.speed = 77
+      return self
+    elif speed == 3 and self.Type == "saloon":
+      self.speed = 1000
+      return self
+    return self
+    
